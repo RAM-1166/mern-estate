ff6d2a8239f69cd74722191c8a18c224e36bc3bc
ram <rampeteti1166@gmail.com>
2024-03-20 21:55:36 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=> {
 console.log("Connected to mongodb");
@@ -12,3 +13,4 @@ app.listen(3000,() =>{
     console.log('Server running at 3000');
 }
 );
+app.use('/api/user',userRouter);
