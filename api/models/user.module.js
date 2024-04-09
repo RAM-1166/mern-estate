37fd5fa2a5a8eaf47ebc514cc85081e73a6a2356
ram <rampeteti1166@gmail.com>
2024-04-09 17:09:31 +0530

    create Oauth with firebase

--- api/models/user.module.js
+++ api/models/user.module.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar:{
+      type:String,
+      default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGrcE_g5YgDcZyZxIvsOLyb6WYis2lGHmADw&s",
+    },
   },
   { timestamps: true }
 );
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import { set } from 'mongoose';
 import React, { useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom'; 
+import OAuth from '../components/OAuth';
 
 export default function Signup() {
   const [formData,setFormData]=useState({});
@@ -53,6 +54,7 @@ export default function Signup() {
         <button disabled={loading} className='bg-blue-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {loading? 'loading...':'Signup'}
         </button>
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Already have an account?</p>
