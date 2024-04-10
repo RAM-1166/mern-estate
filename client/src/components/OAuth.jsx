168e2f9c7199c397bff66adc3903be02ed52675c
ram <rampeteti1166@gmail.com>
2024-04-10 21:45:09 +0530

    complete image fuctionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {GoogleAuthProvider,getAuth,signInWithPopup} from 'firebase/auth';
 import {useDispatch} from 'react-redux';
 import { signInSuccess } from '../redux/user/userslice';
-import app from '../firebase';
+import {app} from '../firebase';
 import {useNavigate} from 'react-router-dom';
 export default function OAuth() {
     const dispatch =useDispatch();
