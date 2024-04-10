0ca1b39165cd83547ab35547b37cc825759940ad
ram <rampeteti1166@gmail.com>
2024-04-10 22:53:12 +0530

    create update user api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=> {
 console.log("Connected to mongodb");
@@ -11,6 +12,7 @@ console.log("Connected to mongodb");
 });
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000,() =>{
     console.log('Server running at 3000');
 }
