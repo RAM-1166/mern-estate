8dfad97ff45561d74cee36bec417d7c2a8edde01
ram <rampeteti1166@gmail.com>
2024-04-11 12:39:03 +0530

    add create listings api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=> {
 console.log("Connected to mongodb");
@@ -19,6 +20,7 @@ app.listen(3000,() =>{
 );
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err,req,res,next)=>{ //middlewware creation to reduce the size of duplicate blocks
     const statuscode=err.statuscode || 500;
